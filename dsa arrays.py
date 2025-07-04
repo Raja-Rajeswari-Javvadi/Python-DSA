0ae47a318cfc55a740700180a0be322660d47f68
Raja Rajeswari Javvadi <146972116+Raja-Rajeswari-Javvadi@users.noreply.github.com>
2025-07-04 22:38:07 +0530

    Update dsa arrays.py

--- dsa arrays.py
+++ dsa arrays.py
@@ -1,6 +1,6 @@
 arr=[1,2,3,4]
-print(arr)
-print(arr[2])
+print(arr) #basic array
+print(arr[2]) #printing using indexing
 my_arr=[6,3,5,8,2]
-my_arr.sort()
+my_arr.sort() 
 print(my_arr[0])#printing lowest element in the array
